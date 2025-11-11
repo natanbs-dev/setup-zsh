0bfb4ceeb42855b842c67930467d36a7947e60e0
nbs-debian <natanbarbosa50@gmail.com>
2025-11-11 10:59:49 -0300

    edit: espaçamento

--- zsh-bugs/bug-plugin.sh
+++ zsh-bugs/bug-plugin.sh
@@ -7,8 +7,11 @@ echo "quando tiver o erro no terminal de "[oh-my-zsh] zsh-autosuggestions
 sudo rm -rf /home/barbosa/.oh-my-zsh/custom/plugins/zsh-autosuggestions*
 git clone https://github.com/zsh-users/zsh-autosuggestions.git
 source ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+
 echo "finalizando com a saída do terminal; em muitos casos, caso somente rode
 o comando source ~/.zshrc não está consertando, precisa-se sair das sessões
 do terminal"
+
 exit 
+
 echo "funcionando também para demais plugins que tiverem o erro de not found"
